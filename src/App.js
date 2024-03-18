d6a4c8b84e23a7db6ebcdad30762343aab217f80
JoanaQuadzie <joanaq03@gmail.com>
2024-03-18 18:02:25 +0000

    landingreactpage

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <header className="bg">
+          <div className="top-items">
+            {/* <h2 class="icon">Sure Success College</h2> */}
+            <nav>
+              <li>
+                <a href="">HOME</a>
+              </li>
+              <li>
+                <a href="">ABOUT</a>
+              </li>
+              <li>
+                <a href="">CONTACT US</a>
+              </li>
+              <li>
+                <a href="">VISIT US</a>
+              </li>
+            </nav>
+            <div><button className="submit">SIGN UP</button></div>
+          </div>
+        </header>
+
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
