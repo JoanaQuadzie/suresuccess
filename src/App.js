f8d3254af762dbbce738eb5f7fd30ec072fc9855
JoanaQuadzie <joanaq03@gmail.com>
2024-03-19 12:13:14 +0000

    created sure success page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <div>
         <header className="bg">
           <div className="top-items">
-            {/* <h2 class="icon">Sure Success College</h2> */}
+            <h2 class="icon">SURE SUCCESS COLLEGE</h2>
             <nav>
               <li>
                 <a href="">HOME</a>
@@ -22,9 +22,52 @@ function App() {
                 <a href="">VISIT US</a>
               </li>
             </nav>
-            <div><button className="submit">SIGN UP</button></div>
+            <div  className="submit"><button>SIGN UP</button></div>
           </div>
         </header>
+
+
+
+        <section className="services" >
+          <h2>OUR SERVICES</h2>
+
+          <div className="services">
+            <div className="services-card">
+              <h3>MAY/JUNE REGISTRATION</h3>
+
+              <p>Cranberry juice, Beet juice, Grape juice etc</p>
+            </div>
+
+            <div className="services-card">
+              <h3>NOV/DEC REGISTRATION</h3>
+
+              <p>Cranberry juice, Beet juice, Grape juice etc</p>
+            </div>
+
+            <div className="services-card">
+              <h3>DOUBLE & SINGLE TRACK VACATION CLASSES</h3>
+
+              <p>Cranberry juice, Beet juice, Grape juice etc</p>
+            </div>
+
+            <div className="services-card">
+              <h3>REMEDIAL CLASSES</h3>
+
+              <p>Cranberry juice, Beet juice, Grape juice etc</p>
+            </div>
+          </div>
+        </section>
+
+        <section>
+
+
+          <footer className="footer-bg">
+            <div class="copy"><p>&copy; 2024
+              www.SureSuccessCollege.com.
+              All rights reserved</p></div>
+
+          </footer>
+        </section>
 
 
       </div>
